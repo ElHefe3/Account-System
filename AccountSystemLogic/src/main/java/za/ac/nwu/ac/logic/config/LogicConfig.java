0b980d28e2e10e8bbc5f6763e54edaa31cb01443
ElHefe3 <koosob@gmail.com>
2021-10-10 15:11:37 +0200

    fixed dependencies

---
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -0,0 +1,8 @@
+package za.ac.nwu.ac.logic.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@import({TranslatorConfig.class})
+@Configuration
+public class LogicConfig {
+}
---
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/config/TranslatorConfig.java
@@ -0,0 +1,8 @@
+package za.ac.nwu.ac.translator.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@import({RepositoryConfig.class})
+@Configuration
+public class TranslatorConfig {
+}
